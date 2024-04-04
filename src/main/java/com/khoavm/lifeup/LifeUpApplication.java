c05e1d59f9916df6b5b1e065c81d0c561d146bf6
Vũ Minh Khoa <khoavm@MacBook-Pro-cua-Vu.local>
2024-04-05 04:45:51 +0700

    inital project

---
+++ src/main/java/com/khoavm/lifeup/LifeUpApplication.java
@@ -0,0 +1,13 @@
+package com.khoavm.lifeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khoavm/lifeup/LifeUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khoavm.lifeup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
